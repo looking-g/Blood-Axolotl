dba341265ab53f1a7a825ec1eaa965602bad2f0b
Landen <lookinggood.yes@gmail.com>
2026-08-25 20:29:00 -0500

    Set up a basic 2D bevy app

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Blood Axolotl".into(),
+                    name: Some("blood-axolotl.app".into()),
+                    resolution: (1200, 700).into(),
+                    ..default()
+                }),
+                ..default()
+            }),
+        ))
+
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn((
+        Camera2d,
+    ));
+
+
+    commands.spawn((
+        Mesh2d( meshes.add(Rectangle::new(100.0, 100.0)) ),
+        MeshMaterial2d(materials.add( Color::srgba(0.0, 0.0, 0.0, 1.0) )),
+        Transform::from_xyz(0.0, 0.0, 0.0),
+    ));
+
+
+}
